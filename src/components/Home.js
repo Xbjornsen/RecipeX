37994063cf4333505250bad922d5bb43847d1c76
Xbjornsen <xaviertr7@gmail.com>
2021-11-10 15:36:44 +0930

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,7 @@
 import React from "react";
-import { makeStyles } from "@material-ui/styles";
-import { Container } from "@material-ui/core";
-import { Typography } from "@material-ui/core";
-import Add from "./DataBaseComponents/Add";
-import FireStoreData from "./DataBaseComponents/FireStoreData";
+import { makeStyles, styled } from "@material-ui/styles";
+import { Box, Grid, Paper } from "@material-ui/core";
+
 
 
 
@@ -11,26 +9,51 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     flexGrow: 1,
+    margin: 10,
   },
   container: {
     display: "flex",
     postion: "absolute",
     width: "100%",
-    height: "100%",
+    height: "100vh",
+  },
+  heading: {
+    display: "flex",
+    postion: "center",
+  },
+  recipieHeading: {
+    display: "flex",
+    postion: "absolute",
+    fontSize: "14",
   },
 }));
 
 function Home() {
   const classes = useStyles();
 
+  const Item = styled(Paper)(({ theme }) => ({
+    padding: theme.spacing(1),
+    textAlign: 'center',
+  }))
+
   return (
-    <div className={classes.root}>
-        <Container className={classes.container}>
-            <Typography variant='h4'>Home</Typography>
-            
-        </Container>
-        < Add />
-        <FireStoreData /> 
+    <div >
+        <Box className={classes.root} >
+          <Grid container spacing={2}>
+            <Grid item xs={6} md={4}>
+            <Item>hi</Item>
+            </Grid>
+            <Grid item xs={6} md={4}> 
+            <Item></Item>
+            </Grid>
+            <Grid item xs={6} md={4}>
+            <Item></Item>
+            </Grid>
+            <Grid item xs={6} md={8}>
+            <Item></Item>
+            </Grid>
+          </Grid>        
+        </Box> 
     </div>
   );
 }
